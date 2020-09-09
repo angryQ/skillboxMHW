71d52599bd63ae217002ebd0b2e675ec04b3ecb0
Kristina <kudinova2807@gmail.com>
2020-09-10 02:32:39 +0700

    initial

---
+++ DZ7/DZ7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DZ7
+//
+//  Created by Kristina Kudinova on 10.09.2020.
+//  Copyright © 2020 Kristina Kudinova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
