4a29b22fd4849c91c02be0887adace47443bd6a0
Kristina <kudinova2807@gmail.com>
2020-09-12 21:46:38 +0700

    initial

---
+++ DZ8/DZ8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DZ8
+//
+//  Created by Kristina Kudinova on 12.09.2020.
+//  Copyright © 2020 Kristina Kudinova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
