0b92ce132fd6edbc0f8615555a5635184b8207ad
Kristina <kudinova2807@gmail.com>
2020-11-03 23:19:58 +0700

    Набросок UIStepper и UISlider
    Разобраться как сделать чтобы размытие элемента не выходило за пределы UIImage и почему при нажатии на кнопки степпера в некоторых случаях пропускается одна картинка

--- DZ8-additional/DZ8-additional/GalleryViewController.swift
+++ DZ8-additional/DZ8-additional/GalleryViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class GalleryViewController: UIViewController {
     
     @IBOutlet weak var galleryImageView: UIImageView!
-    @IBOutlet weak var nextButton: UIButton!
-    @IBOutlet weak var prevButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
     
+    let animator = UIViewPropertyAnimator(duration: 0.5, curve: .linear)
+    let visualEffectView = UIVisualEffectView(effect: nil)
+    let stepper = UIStepper()
     var currentPicIndex = 0
     let duration: Double = 0.8
+    
     var images = [UIImage(named: "pic-1")!, UIImage(named: "pic-2")!, UIImage(named: "pic-3")!, UIImage(named: "pic-4")!, UIImage(named: "pic-5")!, UIImage(named: "pic-6")!, UIImage(named: "pic-7")!, UIImage(named: "pic-8")!, UIImage(named: "pic-9")!, UIImage(named: "pic-10")!]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,23 +30,28 @@ class GalleryViewController: UIViewController {
         
         galleryImageView.image = images[0]
         galleryImageView.contentMode = .scaleAspectFit
+        galleryImageView.clipsToBounds = true
         startButton.setTitle("Стоп", for: .selected)
+        slider.value = 0
+        
+        configureStepper()
+        configureSlider()
+        
+        
+        animator.addAnimations {
+            //self.galleryImageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            self.visualEffectView.effect = UIBlurEffect(style: .extraLight)
+        }
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupVisualBlurEffectView()
     }
     
-    @IBAction func backButtonTap() {
-        currentPicIndex -= 1
-        if currentPicIndex < 0 {
-            currentPicIndex = images.count - 1
-        }
-        self.galleryImageView.image = images[currentPicIndex]
-    }
-    
-    @IBAction func nextButtonTap() {
-        currentPicIndex += 1
-        if currentPicIndex > images.count - 1 {
-            currentPicIndex = 0
-        }
-        self.galleryImageView.image = images[currentPicIndex]
+    @objc func stepperValueChanged(_ sender:UIStepper!)
+    {
+        self.galleryImageView.image = images[Int(sender.value)]
+        currentPicIndex = Int(sender.value)
     }
     
     @IBAction func startButtonTap(_ sender: UIButton) {
@@ -66,7 +75,11 @@ class GalleryViewController: UIViewController {
     
     func stopAnimationOnImageWithIndex(_ index: Int) {
         galleryImageView.stopAnimating()
-        galleryImageView.image = images[index]
+        if index == 0 {
+            galleryImageView.image = images[images.count - 1]
+        } else {
+            galleryImageView.image = images[index - 1]
+        }
     }
     
     @objc func startAnimationFromImageWithIndex(_ index: Int) {
@@ -74,6 +87,44 @@ class GalleryViewController: UIViewController {
         self.galleryImageView.animationImages = subArray
         self.galleryImageView.animationDuration = Double(subArray.count) * duration
         self.galleryImageView.startAnimating()
+    }
+    
+    func configureSlider() {
+        slider.minimumValue = 0
+        slider.maximumValue = 0.3
+        slider.minimumTrackTintColor = .orange
+        slider.maximumTrackTintColor = .gray
+        slider.thumbTintColor = .black
+        slider.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
+    }
+    
+    @objc func handleSliderChange(sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
+    }
+    
+    func setupVisualBlurEffectView() {
+        view.addSubview(visualEffectView)
+        //guard let height = galleryImageView.image?.size.height, let width = galleryImageView.image?.size.width else {return}
+        visualEffectView.frame = CGRect(x: 20, y: 20, width: galleryImageView.frame.width, height: galleryImageView.frame.height)
+    }
+    
+    func configureStepper() {
+        stepper.setIncrementImage(UIImage(systemName: "bell"), for: .normal)
+        stepper.setDecrementImage(UIImage(systemName: "paperplane.fill"), for: .normal)
+        stepper.translatesAutoresizingMaskIntoConstraints = false
+        stepper.autorepeat = true
+        stepper.tintColor = UIColor.orange
+        stepper.stepValue = 1.0
+        stepper.minimumValue = 0
+        stepper.maximumValue = Double(images.count - 1)
+        stepper.addTarget(self, action: #selector(stepperValueChanged(_:)), for: .valueChanged)
+        self.view.addSubview(stepper)
         
+        //let topConstraint = stepper.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20)
+        let bottomConstraint = stepper.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+        let leadingConstraint = stepper.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 50)
+        let trailingConstraint = stepper.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -50)
+        
+        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, bottomConstraint])
     }
 }
