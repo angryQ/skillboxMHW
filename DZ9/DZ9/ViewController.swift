7e8e2c0e07594ca513c1deee9a8ec5a481e5a8fb
Kristina <kudinova2807@gmail.com>
2020-11-16 11:34:13 +0700

    Добавила правки по рекомендациям Дмитрия

--- DZ9/DZ9/ViewController.swift
+++ DZ9/DZ9/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var orangeTextField: UITextField!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var orangeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let unwrappedGreenLabel = greenLabel, let unwrappedOrangeLabel =  orangeLabel else { return }
+        unwrappedGreenLabel.text = greenTextField.text
+        unwrappedOrangeLabel.text = orangeTextField.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
