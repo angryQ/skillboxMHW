7276eb6b39ff3564584aa332e139ac1d4551c263
Kristina <kudinova2807@gmail.com>
2021-01-19 15:23:26 +0700

    Initial

---
+++ DZ10-dop/DZ10-dop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DZ10-dop
+//
+//  Created by Kristina Kudinova on 19.01.2021.
+//  Copyright © 2021 Kristina Kudinova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
